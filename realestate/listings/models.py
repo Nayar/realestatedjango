689ef0e1d6f3ab40a561cab8d0b5b95a561f6479
Nayar Joolfoo <nayar@joolfoo.com>
2022-09-20 11:23:52 +0400

    added listings on the home page

--- realestate/listings/models.py
+++ realestate/listings/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class House(models.Model):
+    logo = models.ImageField(upload_to="images/")
+    address1 = models.CharField(max_length=100)
+    address2 = models.CharField(max_length=100)
+    price = models.IntegerField()
+    garage = models.IntegerField()
+    bedroom = models.IntegerField()
+    size = models.IntegerField(max_length=50)
--- realestate/listings/views.py
+++ realestate/listings/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import House
 
 # Create your views here.
 def index(request):
-
+    # Get all houses
+    houses = House.objects.all() 
+    context={'houses': houses}
     # render page from "templates/index.html"
-    return render(request,"index.html",context={})
+    return render(request,"index.html",context)
