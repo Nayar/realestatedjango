6808fa8487c7266044a4fdbfa97f3b56f2521167
Nayar Joolfoo <nayar@joolfoo.com>
2022-09-22 10:48:38 +0400

    made filter by bedroom

--- realestate/listings/views.py
+++ realestate/listings/views.py
@@ -3,8 +3,14 @@ from .models import House
 
 # Create your views here.
 def index(request):
-    # Get all houses
-    houses = House.objects.all() 
+    try:
+        if(int(request.GET['bedroom']) > 0):
+            houses = House.objects.filter(bedroom=request.GET['bedroom']) 
+        else:
+            houses = House.objects.all()
+    except:
+        # Get all houses
+        houses = House.objects.all() 
     context={'houses': houses}
     # render page from "templates/index.html"
     return render(request,"index.html",context)
